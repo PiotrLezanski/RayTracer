8b60776107b831a9dae953f18bf361c021c2e5e0
Piotr Lezanski <piotr.lezanski@autodesk.com>
2025-03-16 13:29:39 +0100

    add padding

--- src/utils/Interval.h
+++ src/utils/Interval.h
@@ -9,14 +9,20 @@ struct Interval
     Interval(double min, double max) : m_min(min), m_max(max) {}
 
     double size() const { return m_max - m_min; }
-
     bool contains(double x) const { return m_min <= x && x <= m_max; }
-
     bool surrounds(double x) const { return m_min < x && x < m_max; }
+    void addPadding(double);
 
     double m_min;
     double m_max;
 };
+
+void Interval::addPadding(double p)
+{
+    const double padding = p / 2;
+    m_min -= padding;
+    m_max += padding;
+}
 
 inline const Interval EMPTY_INTERVAL = Interval(+INF, -INF);
 inline const Interval UNIVERSE_INTERVAL = Interval(-INF, +INF);
